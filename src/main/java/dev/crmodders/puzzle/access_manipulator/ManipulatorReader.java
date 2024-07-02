9193c13d497cfe41aaca612ccfa5b3458720fe7c
repletsin5 <50048371+repletsin5@users.noreply.github.com>
2024-07-02 20:34:02 +0100

    AM reader

--- src/main/java/dev/crmodders/puzzle/access_manipulator/ManipulatorReader.java
+++ src/main/java/dev/crmodders/puzzle/access_manipulator/ManipulatorReader.java
@@ -1,9 +1,78 @@
 package dev.crmodders.puzzle.access_manipulator;
 
+import dev.crmodders.puzzle.access_manipulator.pairs.FieldModifierPair;
+import dev.crmodders.puzzle.access_manipulator.pairs.MethodModifierPair;
+import dev.crmodders.puzzle.access_manipulator.transformers.AccessManipulatorTransformer;
+import dev.crmodders.puzzle.access_manipulator.transformers.ClassModifier;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.*;
+import java.util.regex.Pattern;
+
 public class ManipulatorReader {
 
-    public static Object readManipulator(String contents) {
-        return null;
+
+    public static void read(String contents){
+        try {
+            readManipulator(contents);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public static void readManipulator(String contents) throws IOException {
+        BufferedReader reader =  new BufferedReader(new StringReader(contents));
+        String ln;
+        while((ln = reader.readLine())!=null){
+            if(ln.isBlank() || ln.isEmpty())
+                continue;
+            List<String> tokens = Arrays.asList(Pattern.compile("[ \\t]+").split(ln));
+            ClassModifier modifier;
+            var access = tokens.get(0);
+            switch (access){
+                case "public":
+                    modifier = ClassModifier.PUBLIC;
+                    break;
+                case"private":
+                    modifier = ClassModifier.PRIVATE;
+                    break;
+                case "protected":
+                    modifier = ClassModifier.PROTECTED;
+                    break;
+                default:
+                    throw new RuntimeException("Unsupported access: '" + tokens.get(0) + "'");
+            }
+            var type = tokens.get(1);
+            switch (type) {
+                case "class":
+                    if (tokens.size()==3)
+                        AccessManipulatorTransformer.classesToModify.put(tokens.get(2),modifier);
+                    else
+                        throw new RuntimeException("Layout is invalid for class AM");
+                    break;
+                case "field":
+                    if (tokens.size()==4) {
+                        HashMap<String, FieldModifierPair> hm = new HashMap<>();
+                        hm.put(tokens.get(2),new FieldModifierPair(tokens.get(3), tokens.get(2), modifier));
+                        AccessManipulatorTransformer.fieldsToModify.put(tokens.get(3), hm);
+                    }
+                    else
+                        throw new RuntimeException("Layout is invalid for field AM");
+                    break;
+                case "method":
+                    if(tokens.size()==5){
+                        List<MethodModifierPair> p = new ArrayList<>(1);
+                        p.add(new MethodModifierPair(tokens.get(3),tokens.get(4),tokens.get(2),modifier ));
+                        AccessManipulatorTransformer.methodsToModify.put(tokens.get(2),p);
+                    }else {
+                        throw new RuntimeException("Layout is invalid for method AM");
+                    }
+                    break;
+                default:
+                    throw new RuntimeException("Unsupported type: '" + tokens.get(1) + "'");
+            }
+        }
     }
 
 }
