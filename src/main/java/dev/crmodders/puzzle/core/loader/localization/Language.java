201e920416a88b29406506a690e2cd7b92aab0f5
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2024-07-15 19:45:35 -0400

    hmmm

---
+++ src/main/java/dev/crmodders/puzzle/core/loader/localization/Language.java
@@ -0,0 +1,16 @@
+package dev.crmodders.puzzle.core.loader.localization;
+
+public class Language {
+
+    public static Language defaultGameLang;
+    public static Language currentLanguage;
+
+    //private
+
+    /*public Language() {
+
+    }
+
+    public static void registerLanguage(String file, ) {
+    }*/
+}
