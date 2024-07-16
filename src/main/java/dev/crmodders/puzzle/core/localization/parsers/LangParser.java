89e1e4a6834eab7f65ca8652f0a312704bdcb783
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2024-07-16 12:59:12 -0400

    Update LangParser.java

--- src/main/java/dev/crmodders/puzzle/core/localization/parsers/LangParser.java
+++ src/main/java/dev/crmodders/puzzle/core/localization/parsers/LangParser.java
@@ -1,10 +1,7 @@
 package dev.crmodders.puzzle.core.localization.parsers;
 
-import dev.crmodders.puzzle.core.localization.Language;
-
 public interface LangParser {
-
-    Language parse(String source);
+    ParseResult parse(String source);
     // maybe not pass source?
-    boolean canParse(String fileName, String source);
+    ParseResult canParse(String fileName, String source);
 }
