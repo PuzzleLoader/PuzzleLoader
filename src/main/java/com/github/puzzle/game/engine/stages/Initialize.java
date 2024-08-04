4856b374e2543aca665c28786c31505e0e0f63f1
Mr Zombii <thezombiigames@gmail.com>
2024-08-03 20:25:58 -0500

    add counting to mod loading screen

--- src/main/java/com/github/puzzle/game/engine/stages/Initialize.java
+++ src/main/java/com/github/puzzle/game/engine/stages/Initialize.java
@@ -7,6 +7,8 @@ import com.github.puzzle.loader.entrypoint.interfaces.ModInitializer;
 import com.github.puzzle.loader.mod.ModContainer;
 import com.github.puzzle.loader.mod.ModLocator;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Initialize extends LoadStage {
 
     @Override
@@ -22,22 +24,23 @@ public class Initialize extends LoadStage {
 
         if (ModLocator.locatedMods == null) ModLocator.getMods();
 
-        int progress = 0;
+        AtomicInteger progress = new AtomicInteger();
         loader.setupProgressBar(loader.progressBar2, ModLocator.locatedMods.size(), "Initializing Mods: Init");
         ModLocator.locatedMods.keySet().forEach(containerID -> {
             ModContainer container = ModLocator.locatedMods.get(containerID);
             int counterLimiter = ModLocator.locatedMods.size() >= 100 ? 10 : 1;
             try {
                 if (counter >= counterLimiter) {
-                    String str = "Loading Mod: " + container.NAME + " | %d/%d";
+                    String str = "Loading Mod: " + container.NAME + " | " + progress.get() + "/" + ModLocator.locatedMods.size();
                     loader.progressBarText2.setText(str);
-                    loader.progressBar2.setValue(progress);
+                    loader.progressBar2.setValue(progress.get());
                     counter = 0;
                 } else counter++;
                 container.invokeEntrypoint(ModInitializer.ENTRYPOINT_KEY, ModInitializer.class, ModInitializer::onInit);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
+            progress.getAndIncrement();
         });
 
     }
