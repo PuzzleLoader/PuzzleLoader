d1015d7a1c536bd3dfd9455dafb8f518cac2c09c
SinfullySoul <amills01082000@gmail.com>
2024-08-17 22:16:35 -0400

    getSlotsFromDirection now takes in Direction

--- src/main/java/com/github/puzzle/game/items/IStorageContainer.java
+++ src/main/java/com/github/puzzle/game/items/IStorageContainer.java
@@ -8,7 +8,6 @@ import finalforeach.cosmicreach.items.ItemSlot;
 public interface IStorageContainer {
     default ItemSlot[] getInputSlots() { return null; }
     default ItemSlot[] getOutputSlots() { return null; }
-    default ItemSlot[] getSlotsFromDirection(BlockPosition blockPosition) { return null; }
+    default ItemSlot[] getSlotsFromDirection(Direction direction) { return null; }
     default ISlotContainer getContainer() { return null; }
 }
-Direction direction
