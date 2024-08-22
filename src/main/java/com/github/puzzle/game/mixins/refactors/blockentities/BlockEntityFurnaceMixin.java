54bc0123197c8029f155c737afdaf6ab019b5ecc
SinfullySoul <amills01082000@gmail.com>
2024-08-22 14:39:08 -0400

    implements IStorageContainer to BlockEntityFurnace

---
+++ src/main/java/com/github/puzzle/game/mixins/refactors/blockentities/BlockEntityFurnaceMixin.java
@@ -0,0 +1,33 @@
+package com.github.puzzle.game.mixins.refactors.blockentities;
+
+import com.github.puzzle.game.items.IStorageContainer;
+import finalforeach.cosmicreach.blockentities.BlockEntityFurnace;
+import finalforeach.cosmicreach.constants.Direction;
+import finalforeach.cosmicreach.items.ISlotContainer;
+import finalforeach.cosmicreach.items.ItemSlot;
+import finalforeach.cosmicreach.items.containers.FurnaceSlotContainer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(BlockEntityFurnace.class)
+public class BlockEntityFurnaceMixin implements IStorageContainer {
+
+    @Shadow
+    public FurnaceSlotContainer slotContainer;
+
+    @Override
+    public ItemSlot[] getInputSlots() {
+        ItemSlot[] Slots = new ItemSlot[2];
+        Slots[0] = slotContainer.getFuelSlot();
+        Slots[1] = slotContainer.getIngredientSlot();
+        return Slots;
+    }
+
+    @Override
+    public ItemSlot[] getOutputSlots() {
+        ItemSlot[] Slots = new ItemSlot[2];
+        Slots[0] = slotContainer.getOutputProductSlot();
+        Slots[1] = slotContainer.getOutputByProductSlot();
+        return Slots;
+    }
+}
