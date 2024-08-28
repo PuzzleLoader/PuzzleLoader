d50db77a94ea824b85e793cc090f4b4944d712b2
SinfullySoul <amills01082000@gmail.com>
2024-08-27 22:08:50 -0400

    updated Modmenu so sexy

--- src/main/java/com/github/puzzle/game/ui/ModMenu.java
+++ src/main/java/com/github/puzzle/game/ui/ModMenu.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -17,20 +18,27 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.github.puzzle.game.engine.GameLoader;
 import com.github.puzzle.game.ui.font.CosmicReachFont;
 import com.github.puzzle.loader.mod.ModContainer;
 import com.github.puzzle.loader.mod.ModLocator;
 import com.github.puzzle.loader.mod.info.ModInfo;
+import com.google.common.collect.ImmutableMap;
 import finalforeach.cosmicreach.GameAssetLoader;
 import finalforeach.cosmicreach.gamestates.GameState;
 import finalforeach.cosmicreach.gamestates.MainMenu;
 import finalforeach.cosmicreach.gamestates.PauseMenu;
+import org.hjson.JsonObject;
+import org.hjson.JsonValue;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 
-public class ModMenu extends GameState {
+import java.util.Collection;
 
-    public static NinePatch backgoundimg = new NinePatch(new Texture(GameAssetLoader.loadAsset("assets/puzzle-loader/icons/PuzzleLoaderIconx32.png")));
+import static com.github.puzzle.core.resources.PuzzleGameAssetLoader.LOADER;
+import static com.github.puzzle.game.ui.font.CosmicReachFont.createCosmicReachFont;
+
+public class ModMenu extends GameState {
 
     Stage gdxStage;
     Viewport gdxStageViewport;
@@ -38,21 +46,16 @@ public class ModMenu extends GameState {
 
     ScrollPane scrollPane;
     Button.ButtonStyle buttonStyle;
-    Table modPage;
-    Drawable backgroundTopBottom;
-    Drawable modListBar;
+    BitmapFont modlablefont = createCosmicReachFont();
+    BitmapFont versonfont = createCosmicReachFont();
 
     public ModMenu(GameState currentGameState) {
         this.previousState = currentGameState;
         buttonStyle = new Button.ButtonStyle();
-        Pixmap pm = new Pixmap(1,1, com.badlogic.gdx.graphics.Pixmap.Format.RGBA8888);
-        pm.setColor(new Color(.10f,  .10f,  .10f, 1));
-        pm.fill();
-        backgroundTopBottom = new TextureRegionDrawable(new Texture(pm));
-        pm.setColor(new Color(.10f,  .10f,  .10f, 1));
-        pm.fill();
-        modListBar = new TextureRegionDrawable(new Texture(pm));
-        pm.dispose();
+        buttonStyle.up = genColor(.231f,.231f, .231f);
+        buttonStyle.down = genColor(.331f,.331f, .331f);
+        versonfont.getData().setScale(0.6F);
+        modlablefont.getData().setScale(0.9F);
     }
 
     @Override
@@ -63,75 +66,55 @@ public class ModMenu extends GameState {
         gdxStage = new Stage(gdxStageViewport, batch);
         gdxStageCamera.position.set(0, 0, 0);
         Gdx.input.setInputProcessor(gdxStage);
-        modPage = new Table();
 
-        Table bottomBarLeft = new Table();
-        Table bottomBarRight = new Table();
-        Table topBarLeft = new Table();
-        Table topBarRight = new Table();
-        bottomBarLeft.background(backgroundTopBottom);
-        bottomBarRight.background(backgroundTopBottom);
-        topBarLeft.background(backgroundTopBottom);
-        topBarRight.background(backgroundTopBottom);
-
-        Table modPane = new Table();
-        for(ModContainer m : ModLocator.locatedMods.values()) {
-            Button button = new Button(buttonStyle);
-            button.addListener(getClickListener(m, modPage));
-            button.add(new Image(backgoundimg)).left().padLeft(5);
-            button.add(new Label(m.NAME, new Label.LabelStyle(CosmicReachFont.FONT, Color.WHITE))).expand().center().padTop(15);
-            modPane.add(button).minWidth(160).minHeight(50).pad(5);
-            modPane.row();
-        }
-
-        scrollPane = new ScrollPane(modPane);
+        Table modPanel = new Table();
+        scrollPane = new ScrollPane(modPanel);
         scrollPane.setScrollingDisabled(true, false);
         scrollPane.setSmoothScrolling(true);
-        Container container = new Container(scrollPane);
-        container.fill();
-        container.padLeft(15).padRight(15);
-        container.background(modListBar);
+        Table baseTable = new Table();
+        Table leftTable = new Table();
+        leftTable.background(genColor(.141f, .141f, .141f));
+        Table righTable = new Table();
+        righTable.background(genColor(.31f, .31f, .31f));
 
-        Table root = new Table();
-        Table topBar = root.add(topBarLeft).minHeight(40).maxHeight(40).maxWidth(160).fill().getActor();
-        root.add(topBarRight).minHeight(40).maxHeight(40).top().left().expandX().fill();
-        root.row();
+        for(ModContainer m : ModLocator.locatedMods.values()) {
+            String path = m.INFO.Metadata.get("icon") != null ? m.INFO.Metadata.get("icon").asString() : "assets/puzzle-loader/icons/example.png";
+            Image icon = new Image(LOADER.loadSync(path, Texture.class));
+            Table rightSide = new Table();
+            Label modname = new Label(m.NAME, new Label.LabelStyle(modlablefont, Color.WHITE));
+            Label modversion = new Label("v"+m.VERSION.toString() , new Label.LabelStyle(versonfont, Color.WHITE));
+            modname.setAlignment(Align.bottom);
+            modversion.setAlignment(Align.center);
+            Button button = new Button(buttonStyle);
+            button.addListener(getClickListener(m, righTable));
+            button.add(icon).width(32).height(32).left().expandY().fill();
+            button.add(rightSide).padLeft(4).expand().fill();
+            rightSide.add(modname).height(25).left().bottom().expandX().row();
+            rightSide.add(modversion).height(25).left().padTop(3).bottom().expandX();
+            modPanel.add(button).minWidth(150).minHeight(50).pad(5).top().fill();
+            modPanel.row();
+        }
+        modPanel.add().grow();
 
-        root.add(container).width(150).maxWidth(160).left().fill().expandY();
-        root.add(modPage).expand().pad(10).fill();
-        root.row();
-
-        root.add(bottomBarLeft).minHeight(40).maxHeight(40).maxWidth(160).fill();
-        root.add(bottomBarRight).minHeight(40).maxHeight(40).bottom().left().expandX().fill();
-        root.setFillParent(true);
-
-        Label title = new Label("MODS", new Label.LabelStyle(CosmicReachFont.FONT_BIG, Color.BLACK));
-        title.layout();
+        Label title = new Label("Mods", new Label.LabelStyle(CosmicReachFont.FONT_BIG, Color.WHITE));
         title.setSize(title.getGlyphLayout().width, title.getGlyphLayout().height);
-        title.setPosition(200.0F, 100.0F, Align.center);
-        gdxStage.addActor(root);
-        gdxStage.setScrollFocus(scrollPane);
+        title.setAlignment(Align.center);
 
         Button backButton = new Button(buttonStyle);
         backButton.add(new Label("Done", new Label.LabelStyle(CosmicReachFont.FONT, Color.WHITE))).expand().center().padTop(15);
+        backButton.background(genColor(.231f,.231f, .231f));
         backButton.addListener(getClickListener(this));
-        bottomBarRight.add(backButton).right().expandX().padRight(20);
-        //scrollPane.debugAll();
-        //bottomBarRight.debugAll();
-        //root.debugAll();
-        //bottemBar.debugAll();
-        //modPage.debugAll();
 
-//        UIElement doneButton = new UIElement(0.0F, 200.0F, 250.0F, 50.0F) {
-//            public void onClick() {
-//                super.onClick();
-//                ModMenu.this.returnToPrevious();
-//            }
-//        };
-//        doneButton.setText(Lang.get("doneButton"));
-//        doneButton.show();
-//
-//        uiObjects.add(doneButton);
+        leftTable.add(title).width(leftTable.getWidth()).padTop(50).padBottom(10).center().fill().row();
+        leftTable.add(scrollPane).center().expand().fill().row();
+        leftTable.add(backButton).height(30).width(backButton.getPrefWidth() + 10).bottom().padBottom(5);
+
+        baseTable.add(leftTable).left().width(160).expandY().fill();
+        baseTable.add(righTable).pad(10).expand().fill();
+        baseTable.setFillParent(true);
+        //righTable.debugAll();
+        gdxStage.addActor(baseTable);
+        gdxStage.setScrollFocus(scrollPane);
     }
 
     private void returnToPrevious() {
@@ -150,7 +133,7 @@ public class ModMenu extends GameState {
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             this.returnToPrevious();
         }
-        Gdx.gl.glClearColor( .20f,  .20f,  .20f, 1);
+        Gdx.gl.glClearColor( 0.212f,  0.212f,  0.212f, 1);
         Gdx.gl.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
         gdxStageViewport.apply(true);
         gdxStage.act(Gdx.graphics.getDeltaTime());
@@ -160,9 +143,7 @@ public class ModMenu extends GameState {
 
     @Override
     public void resize(int width, int height) {
-        //super.resize(width, height);
         gdxStage.getViewport().update(width, height, true);
-        backgroundTopBottom.setMinWidth(width);
     }
 
     @Override
@@ -183,28 +164,51 @@ public class ModMenu extends GameState {
     }
 
     private ClickListener getClickListener(ModContainer mod, Table table){
-        TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle();
-        textFieldStyle.font = CosmicReachFont.FONT;
-        textFieldStyle.fontColor = Color.WHITE;
 
         return new ClickListener(){
             public void clicked(InputEvent event, float x, float y){
-                ModInfo info = mod.INFO;
-                String str = switch (mod.NAME) {
-                    case "Puzzle Loader" -> "Mod Loader";
-                    case "Cosmic Reach" -> "Base";
-                    default -> "Mod";
-                };
                 table.clear();
-                table.add(new Label(str + ": " + mod.NAME, new Label.LabelStyle(CosmicReachFont.FONT, Color.WHITE))).height(30).width(250).top().left().fill().row();
-                table.add(new Label("Version: [" + mod.VERSION.toString() + "]", new Label.LabelStyle(CosmicReachFont.FONT, Color.WHITE))).height(30).width(250).top().left().fill().row();
-                table.add(new Label(info.Description, new Label.LabelStyle(CosmicReachFont.FONT, Color.WHITE))).left().expandY().pad(5).fill();
-                table.add(new Table()).expand();
-                ((Label)table.getChild(0)).setAlignment(Align.bottom);
-                ((Label)table.getChild(1)).setAlignment(Align.bottom);
-                Label label = ((Label)table.getChild(2));
-                label.setAlignment(Align.center);
-                label.setWrap(true);
+                ModInfo info = mod.INFO;
+                ScrollPane bottomBar = mod.getCustomModTable() != null ? new ScrollPane(mod.getCustomModTable()) : new ScrollPane(getDefaultTable());
+                Table topBar = new Table();
+                Table topRightBar = new Table();
+                topRightBar.setBackground(genColor(.196f,.196f, .196f));
+                String path = info.Metadata.get("icon") != null ? info.Metadata.get("icon").asString() : "assets/puzzle-loader/icons/example.png";
+                Image icon = new Image(LOADER.loadSync(path, Texture.class));
+                Label modname = new Label(mod.NAME, new Label.LabelStyle(CosmicReachFont.FONT_BIG, Color.WHITE));
+                modname.setAlignment(Align.bottomLeft);
+                Label descriptionLabel = new Label(info.Description, new Label.LabelStyle(modlablefont, Color.WHITE));
+                descriptionLabel.setAlignment(Align.center);
+                descriptionLabel.setWrap(true);
+
+                Container<Image> container1 = new Container<Image>(icon).center().height(180).width(180);
+                container1.setBackground(genColor(.196f,.196f, .196f));
+                Container<Label> container3 = new Container<>(descriptionLabel).fillX();
+                container3.pad(10);
+                container3.setBackground(genColor(.196f,.196f, .196f));
+                Container<ScrollPane> scrollContainer = new Container<>(bottomBar).fill();
+                scrollContainer.setBackground(genColor(.196f,.196f, .196f));
+
+                String str = "  Authors: ";
+                String[] authors = info.Authors.toArray(new String[0]);
+                for (int i = 0; i < authors.length; i++) {
+                    str += authors[i];
+                    if(i < authors.length - 1) str += ", ";
+                }
+                Label authorsLabel = new Label(str, new Label.LabelStyle(modlablefont, Color.WHITE));
+                Container<Label> authorsLabelContainer = new Container<>(authorsLabel).fill();
+                authorsLabelContainer.setBackground(genColor(.231f,.231f,.231f));
+
+                topRightBar.add(modname).height(100).pad(10).expandX().fill().row();
+                topRightBar.add(authorsLabelContainer).pad(10).expand().fill();
+                //topRightBar.debugAll();
+
+                topBar.add(container1).height(200).width(200).left().pad(10);
+                topBar.add(topRightBar).expand().fill().padRight(10).padTop(10).padBottom(10).row();
+
+                table.add(topBar).top().expandX().fill().row();
+                table.add(container3).height(80).expandX().fill().padRight(10).padLeft(10).row();
+                table.add(scrollContainer).expand().fill().pad(10);
             }
         };
     }
@@ -215,5 +219,20 @@ public class ModMenu extends GameState {
                 GameState.switchToGameState(modMenu.previousState);
             }
         };
+    }
+
+    private TextureRegionDrawable genColor(float v1, float v2, float v3) {
+        Pixmap pm = new Pixmap(1,1, com.badlogic.gdx.graphics.Pixmap.Format.RGBA8888);
+        pm.setColor(new Color(v1,  v2,  v3, 1));
+        pm.fill();
+        TextureRegionDrawable textureRegionDrawable = new TextureRegionDrawable(new Texture(pm));
+        pm.dispose();
+        return textureRegionDrawable;
+    }
+
+    private Table getDefaultTable(){
+        Table table = new Table();
+
+        return table;
     }
 }
