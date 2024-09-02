c3768c317c8ea5421f5276e75d92f187f63d62d8
Mr Zombii <thezombiigames@gmail.com>
2024-09-02 15:21:38 -0500

    removed unneeded puzzle item renderer constants

--- src/main/resources/assets/puzzle-loader/shaders/item_shader.vert.glsl
+++ src/main/resources/assets/puzzle-loader/shaders/item_shader.vert.glsl
@@ -13,7 +13,7 @@ out vec3 v_normal;
 void main()
 {
 
-	v_normal = normalize(a_normal ); // Mult by u_modelMat for world space vectors
+	v_normal = normalize(a_normal); // Mult by u_modelMat for world space vectors
 	v_texCoord0 = a_texCoord0;
 	gl_Position = u_projViewTrans * u_modelMat * vec4(a_position, 1.0);
 }
