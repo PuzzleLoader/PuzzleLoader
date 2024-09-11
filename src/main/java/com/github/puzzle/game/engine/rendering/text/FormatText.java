6af4831775a20bc9726e1c128774cc10a608f36e
repletsin5 <50048371+repletsin5@users.noreply.github.com>
2024-09-11 01:01:09 +0100

    fix java stupid encoding

--- src/main/java/com/github/puzzle/game/engine/rendering/text/FormatText.java
+++ src/main/java/com/github/puzzle/game/engine/rendering/text/FormatText.java
@@ -3,14 +3,15 @@ package com.github.puzzle.game.engine.rendering.text;
 import com.badlogic.gdx.graphics.Color;
 
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
 public class FormatText {
     public static final String FORMAT_KEY = "§";
-    public static final Pattern FORMAT_PATTER = Pattern.compile("(?i)"+FORMAT_KEY+"[0-9A-FR]");
 
+    public static final Pattern FORMAT_PATTER = Pattern.compile("(?i)"+FORMAT_KEY+"[0-9A-FR]");
 
     public static FormatText of(String text){
         return new FormatText(text).parse();
@@ -33,10 +34,11 @@ public class FormatText {
     }
 
     public FormatText parse() {
+        String fixedText = new String(text.getBytes(StandardCharsets.UTF_8));
         if (!hasParsed) {
-            if (text.contains(FORMAT_KEY)) {
-                var matcher = FORMAT_PATTER.matcher(text);
-                var split = FORMAT_PATTER.split(text);
+            if (fixedText.contains(FORMAT_KEY)) {
+                var matcher = FORMAT_PATTER.matcher(fixedText);
+                var split = FORMAT_PATTER.split(fixedText);
                 if (split.length > 1) {
                     while (matcher.find()) {
                         var s = matcher.group(0);
