f6f932c5eb2dff3f0100d12dfdd970a5638c10c7
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2024-09-15 17:44:55 -0400

    remove some duplicated code in ModLocator.java and PuzzleEntrypointUtil.java, add support for version ranges (smallest...largest) in VersionParser.java, add distance in Vec3i.java

--- src/main/java/com/github/puzzle/util/PuzzleEntrypointUtil.java
+++ src/main/java/com/github/puzzle/util/PuzzleEntrypointUtil.java
@@ -1,6 +1,5 @@
 package com.github.puzzle.util;
 
-import com.github.puzzle.loader.mod.ModContainer;
 import com.github.puzzle.loader.mod.ModLocator;
 
 import java.util.function.Consumer;
@@ -8,10 +7,9 @@ import java.util.function.Consumer;
 public class PuzzleEntrypointUtil {
     public static <T> void invoke(String key, Class<T> entrypointType, Consumer<? super T> entrypointInvoker) {
         if (ModLocator.locatedMods == null) ModLocator.getMods();
-        ModLocator.locatedMods.keySet().forEach(containerID -> {
-            ModContainer container = ModLocator.locatedMods.get(containerID);
+        ModLocator.locatedMods.values().forEach(modContainer -> {
             try {
-                container.invokeEntrypoint(key, entrypointType, entrypointInvoker);
+                modContainer.invokeEntrypoint(key, entrypointType, entrypointInvoker);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
