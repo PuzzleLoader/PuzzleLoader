3fa6fb6e43c3fff056f4cb4fa7de22e3a65e0cbc
Mr Zombii <thezombiigames@gmail.com>
2024-09-15 17:35:59 -0500

    remove old puzzle BE systems

--- src/main/java/com/github/puzzle/game/blockentities/INeighborUpdateListener.java
+++
@@ -1,9 +0,0 @@
-package com.github.puzzle.game.blockentities;
-
-import finalforeach.cosmicreach.blockentities.BlockEntity;
-import finalforeach.cosmicreach.blocks.BlockState;
-import finalforeach.cosmicreach.constants.Direction;
-
-public interface INeighborUpdateListener {
-    void onNeighborUpdate(Direction face, BlockState block, BlockEntity entity);
-}
