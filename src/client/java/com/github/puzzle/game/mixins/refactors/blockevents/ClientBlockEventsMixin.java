cc41d65b4e84b86cfa54aabd32cd4ed041f5e292
SinfullySoul <amills01082000@gmail.com>
2024-11-07 22:32:17 -0500

    add Patch to allow client to get player from BlockEventArgs

---
+++ src/client/java/com/github/puzzle/game/mixins/refactors/blockevents/ClientBlockEventsMixin.java
@@ -0,0 +1,69 @@
+package com.github.puzzle.game.mixins.refactors.blockevents;
+
+import com.github.puzzle.core.loader.launch.Piece;
+import com.github.puzzle.core.loader.meta.EnvType;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import finalforeach.cosmicreach.ClientBlockEvents;
+import finalforeach.cosmicreach.blockevents.BlockEventArgs;
+import finalforeach.cosmicreach.blocks.BlockPosition;
+import finalforeach.cosmicreach.entities.player.Player;
+import finalforeach.cosmicreach.networking.GamePacket;
+import finalforeach.cosmicreach.networking.NetworkIdentity;
+import finalforeach.cosmicreach.world.Zone;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientBlockEvents.class)
+public class ClientBlockEventsMixin {
+
+    @ModifyExpressionValue(method = "breakBlock", at=@At(value = "NEW", target = "()Lfinalforeach/cosmicreach/blockevents/BlockEventArgs;"))
+    private BlockEventArgs breakBlockHelper(BlockEventArgs original){
+        if (Piece.getSide() != EnvType.SERVER) {
+            original.srcIdentity = new NetworkIdentity(null) {
+                @Override
+                public void send(GamePacket gamePacket) {
+                }
+
+                @Override
+                public Player getPlayer() {
+                    return null;
+                }
+            };
+        }
+        return original;
+    }
+    @ModifyExpressionValue(method = "placeBlock", at=@At(value = "NEW", target = "()Lfinalforeach/cosmicreach/blockevents/BlockEventArgs;"))
+    private BlockEventArgs placeBlockHelper(BlockEventArgs original){
+        if (Piece.getSide() != EnvType.SERVER) {
+            original.srcIdentity = new NetworkIdentity(null) {
+                @Override
+                public void send(GamePacket gamePacket) {
+                }
+
+                @Override
+                public Player getPlayer() {
+                    return null;
+                }
+            };
+        }
+        return original;
+    }
+    @ModifyExpressionValue(method = "interactWith", at=@At(value = "NEW", target = "()Lfinalforeach/cosmicreach/blockevents/BlockEventArgs;"))
+    private BlockEventArgs interactWithHelper(BlockEventArgs original){
+        if (Piece.getSide() != EnvType.SERVER) {
+            original.srcIdentity = new NetworkIdentity(null) {
+                @Override
+                public void send(GamePacket gamePacket) {
+                }
+
+                @Override
+                public Player getPlayer() {
+                    return null;
+                }
+            };
+        }
+        return original;
+    }
+}
