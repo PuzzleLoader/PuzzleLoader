52a2751e1ba3f0e31880702f4643224e587b8927
Mr Zombii <thezombiigames@gmail.com>
2024-12-12 17:46:32 -0600

    merge pull request https://github.com/PuzzleLoader/PuzzleLoader/pull/14, change FontRenderer to use FormattedTextRenderer.

--- src/client/java/com/github/puzzle/game/mixins/refactors/ui/MainMenuMixin.java
+++ src/client/java/com/github/puzzle/game/mixins/refactors/ui/MainMenuMixin.java
@@ -1,12 +1,18 @@
 package com.github.puzzle.game.mixins.refactors.ui;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
 import com.github.puzzle.core.localization.LanguageManager;
 import com.github.puzzle.core.localization.TranslationKey;
+import com.github.puzzle.game.common.Puzzle;
+import com.github.puzzle.game.engine.rendering.text.FormattedTextRenderer;
 import com.github.puzzle.game.ui.modmenu.ModMenu;
 import finalforeach.cosmicreach.gamestates.GameState;
 import finalforeach.cosmicreach.gamestates.MainMenu;
+import finalforeach.cosmicreach.ui.FontRenderer;
 import finalforeach.cosmicreach.ui.HorizontalAnchor;
 import finalforeach.cosmicreach.ui.UIElement;
+import finalforeach.cosmicreach.ui.VerticalAnchor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
