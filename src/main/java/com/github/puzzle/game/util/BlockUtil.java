9ceb0e187fe09f7b9b6f3e5277ce79ce4b7c7657
Mr Zombii <thezombiigames@gmail.com>
2024-12-30 21:54:28 -0600

    Update BlockUtil adding BlockReplaceSettings with optional remeshing and light updates.

--- src/main/java/com/github/puzzle/game/util/BlockUtil.java
+++ src/main/java/com/github/puzzle/game/util/BlockUtil.java
@@ -3,20 +3,128 @@ package com.github.puzzle.game.util;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.Vector4;
+import com.badlogic.gdx.utils.Queue;
+import finalforeach.cosmicreach.GameSingletons;
 import finalforeach.cosmicreach.blocks.BlockPosition;
 import finalforeach.cosmicreach.blocks.BlockState;
+import finalforeach.cosmicreach.constants.Direction;
+import finalforeach.cosmicreach.lighting.BlockLightPropagator;
+import finalforeach.cosmicreach.networking.packets.blocks.BlockReplacePacket;
 import finalforeach.cosmicreach.networking.packets.blocks.PlaceBlockPacket;
+import finalforeach.cosmicreach.networking.server.ServerSingletons;
 import finalforeach.cosmicreach.world.BlockSetter;
 import finalforeach.cosmicreach.world.Chunk;
 import finalforeach.cosmicreach.world.Zone;
+import finalforeach.cosmicreach.worldgen.ChunkColumn;
 
 public class BlockUtil {
 
-    public static void setBlockAt(Zone zone, BlockState state, BlockPosition vector3) {
-        if (IClientNetworkManager.isConnected()) {
-            IClientNetworkManager.sendAsClient(new PlaceBlockPacket(zone, vector3, state));
+    public static class BlockReplaceSettings {
+        public static final BlockReplaceSettings DEFAULT = new BlockReplaceSettings();
+
+        boolean doRemeshing = true;
+        boolean doBlockLightPropagation = true;
+        private final boolean isImmutable;
+
+        private BlockReplaceSettings(boolean isImmutable) {
+            this.isImmutable = isImmutable;
         }
-        BlockSetter.get().replaceBlock(zone, state, vector3);
+
+        public BlockReplaceSettings() {
+            isImmutable = false;
+        }
+
+        public BlockReplaceSettings doesRemeshing(boolean doRemeshing) {
+            if (isImmutable) throw new RuntimeException("Cannot set \"doesRemeshing\" on the immutable BlockReplaceSetting");
+
+            this.doRemeshing = doRemeshing;
+            return this;
+        }
+
+        public BlockReplaceSettings doesBlockLightPropagation(boolean doBlockLightPropagation) {
+            if (isImmutable) throw new RuntimeException("Cannot set \"doesBlockLightPropagation\" on the immutable BlockReplaceSetting");
+
+            this.doBlockLightPropagation = doBlockLightPropagation;
+            return this;
+        }
+
+    }
+
+    private static final Queue<BlockPosition> tmpQueue = new Queue<>();
+
+    public static void setBlockAt(Zone zone, BlockState state, BlockPosition pos, BlockReplaceSettings settings) {
+        if (IClientNetworkManager.isConnected()) {
+            IClientNetworkManager.sendAsClient(new PlaceBlockPacket(zone, pos, state));
+        }
+
+        getChunkAtVec(zone, pos.getGlobalX(), pos.getGlobalY(), pos.getGlobalZ());
+
+        BlockState oldBlockState = pos.getBlockState();
+        if (state != oldBlockState) {
+            pos.setBlockState(state);
+            if (settings.doBlockLightPropagation) {
+                adjustLightsAfterReplace(zone, oldBlockState, state, pos, tmpQueue);
+            }
+            if (settings.doRemeshing) {
+                if (state.getModel() != oldBlockState.getModel() && GameSingletons.isClient) {
+                    pos.chunk().flagTouchingChunksForRemeshing(zone, pos.localX(), pos.localY(), pos.localZ(), true);
+                    GameSingletons.meshGenThread.requestImmediateResorting();
+                }
+            }
+        }
+
+        if (GameSingletons.isHost && ServerSingletons.SERVER != null) {
+            ServerSingletons.SERVER.broadcast(zone, new BlockReplacePacket(zone, state, pos));
+        }
+    }
+
+    private static void adjustLightsAfterReplace(Zone zone, BlockState oldBlockState, BlockState targetBlockState, BlockPosition blockPos, Queue<BlockPosition> tmpQueue) {
+        int oldSkylightAttenuation = 0;
+        if (oldBlockState != null) {
+            oldSkylightAttenuation = oldBlockState.lightAttenuation;
+        }
+
+        int currentSkylight = blockPos.getSkyLight();
+        int skylightAttenuation = targetBlockState.lightAttenuation;
+        tmpQueue.clear();
+        tmpQueue.addFirst(blockPos);
+        BlockLightPropagator.propagateBlockDarkness(zone, tmpQueue);
+        tmpQueue.clear();
+        tmpQueue.addFirst(blockPos);
+        BlockLightPropagator.propagateBlockLights(zone, tmpQueue);
+        boolean propagateShade = currentSkylight > 0 && skylightAttenuation > oldSkylightAttenuation;
+        boolean propagateSkylight = currentSkylight != 15 && skylightAttenuation < oldSkylightAttenuation;
+        if (propagateShade || propagateSkylight) {
+            tmpQueue.clear();
+            if (propagateShade) {
+                tmpQueue.addFirst(blockPos);
+                BlockSetter.get().skylightProp.propagateShade(zone, tmpQueue);
+            } else {
+                tmpQueue.addFirst(blockPos.getOffsetBlockPos(zone, Direction.POS_X));
+                tmpQueue.addFirst(blockPos.getOffsetBlockPos(zone, Direction.POS_Y));
+                tmpQueue.addFirst(blockPos.getOffsetBlockPos(zone, Direction.POS_Z));
+                tmpQueue.addFirst(blockPos.getOffsetBlockPos(zone, Direction.NEG_X));
+                tmpQueue.addFirst(blockPos.getOffsetBlockPos(zone, Direction.NEG_Y));
+                tmpQueue.addFirst(blockPos.getOffsetBlockPos(zone, Direction.NEG_Z));
+                BlockSetter.get().skylightProp.propagateSkyLights(zone, tmpQueue);
+            }
+        }
+
+    }
+
+    public static void setBlockAt(Zone zone, BlockState state, BlockPosition pos) {
+        if (IClientNetworkManager.isConnected()) {
+            IClientNetworkManager.sendAsClient(new PlaceBlockPacket(zone, pos, state));
+        }
+        setBlockAt(zone, state, pos, BlockReplaceSettings.DEFAULT);
+    }
+
+    public static void setBlockAt(Zone zone, BlockState state, Vector3 vector3, BlockReplaceSettings settings) {
+        setBlockAt(zone, state, (int) vector3.x, (int) vector3.y, (int) vector3.z, settings);
+    }
+
+    public static void setBlockAt(Zone zone, BlockState state, int x, int y, int z, BlockReplaceSettings settings) {
+        setBlockAt(zone, state, getBlockPosAtVec(zone, x, y, z), settings);
     }
 
     public static void setBlockAt(Zone zone, BlockState state, Vector3 vector3) {
@@ -55,6 +163,7 @@ public class BlockUtil {
         if (c == null) {
             c = new Chunk(cx, cy, cz);
             c.initChunkData();
+            zone.zoneGenerator.generateForChunkColumn(zone, new ChunkColumn(c.chunkX, c.chunkY, c.chunkZ));
             zone.addChunk(c);
         }
 
