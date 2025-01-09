90ac36c39406292a7d51ac687b399e1cc19182d4
Crab_King <127689590+Crab-K1ng@users.noreply.github.com>
2025-01-09 18:13:24 +0000

    fix credits button not going to PuzzleCreditsMenu

--- src/client/java/com/github/puzzle/game/mixins/client/ui/CreditsButtonMixin.java
+++ src/client/java/com/github/puzzle/game/mixins/client/ui/CreditsButtonMixin.java
@@ -3,13 +3,14 @@ package com.github.puzzle.game.mixins.client.ui;
 import com.github.puzzle.game.ui.credits.PuzzleCreditsMenu;
 import finalforeach.cosmicreach.gamestates.GameState;
 import finalforeach.cosmicreach.ui.UIElement;
+import finalforeach.cosmicreach.ui.widgets.CRButton;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(targets = {"finalforeach/cosmicreach/gamestates/MainMenu$6"})
-public class CreditsButtonMixin extends UIElement {
+@Mixin(targets = {"finalforeach/cosmicreach/gamestates/MainMenu$5"})
+public class CreditsButtonMixin extends CRButton {
 
-    public CreditsButtonMixin(float x, float y, float w, float h) {
-        super(x, y, w, h);
+    public CreditsButtonMixin(String labelText) {
+        super(labelText);
     }
 
     @Override
